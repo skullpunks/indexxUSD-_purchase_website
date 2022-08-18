3b54c87a2b6130c4de34ad3918d8bbd1f971a7fa
omkarsaiazooca <omkar@azooca.com>
2022-08-18 20:01:39 +0530

    updated usd+ gif and added water mark

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
-import LogoIcon from "../assets/icons/logo.svg";
+import LogoIcon from "../assets/icons/indexxUSD+.gif";
 
 const Header = () => {
   return (
